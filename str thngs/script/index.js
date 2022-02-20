c801f4856133e5d801e89bb52c29362ca52e6ec7
Biboook <96563744+Biboook@users.noreply.github.com>
2022-02-20 21:16:36 +0600

    Add files via upload

---
+++ str thngs/script/index.js
@@ -0,0 +1,38 @@
+$('button').click(function (){ 
+if(window.scrollY>=200){$('html, body').scrollTop(0);}
+});
+
+$('html').contextmenu(function (e){e.preventDefault();})
+
+var music = document.getElementById("music");
+var isPlaying = false;
+music.volume = 0.2;
+function togglePlay() {
+  if (isPlaying) {
+    music.pause();
+  } else {
+    music.play();
+  }
+}
+music.onplaying = function () {
+  isPlaying = true;
+  document.getElementById("music-animation").classList.add("on");
+};
+music.onpause = function () {
+  isPlaying = false;
+  document.getElementById("music-animation").classList.remove("on");
+};
+
+var button = document.getElementById("toggle");
+button.addEventListener(
+  "click",
+  function () {
+    if (button.getAttribute("data-text-swap") == button.innerHTML) {
+      button.innerHTML = button.getAttribute("data-text-original");
+    } else {
+      button.setAttribute("data-text-original", button.innerHTML);
+      button.innerHTML = button.getAttribute("data-text-swap");
+    }
+  },
+  false
+);
---
+++ str thngs/script/registration.js
@@ -0,0 +1,63 @@
+let form = document.querySelector('.js-form'),
+    inputEmail = document.querySelector('.js-input-email'),
+    inputPassword = document.querySelector('.js-input-password'),
+    inputConfirmPhone = document.querySelector('.js-input-confirmPassword'),
+    inputCheckbox = document.querySelector('.js-input-checkbox');
+
+
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function validateCountry(country) {
+    let re = new RegExp('.co$');
+    return re.test(String(country).toLowerCase());
+}
+
+form.onsubmit = function () {
+    let emailVal = inputEmail.value,
+        phoneVal = inputPhone.value,
+        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+
+        } else {
+            input.classList.remove('error');
+        }
+    });
+
+    if (emptyInputs.length !== 0) {
+        console.log('inputs not filled');
+        return false;
+    }
+
+    if(!validateEmail(emailVal)) {
+        console.log('email not valid');
+        inputEmail.classList.add('error');
+        return false;
+    } else {
+        inputEmail.classList.remove('error');
+    }
+
+    if (validateCountry(emailVal)) {
+        console.log('email from Columbia');
+        inputEmail.classList.add('error');
+        return false;
+    } else {
+        inputEmail.classList.remove('error');
+    }
+
+    if(!inputCheckbox.checked) {
+        console.log('checkbox not checked');
+        inputCheckbox.classList.add('error');
+        return false;
+    } else {
+        inputCheckbox.classList.remove('error')
+    }
+
+
+}
